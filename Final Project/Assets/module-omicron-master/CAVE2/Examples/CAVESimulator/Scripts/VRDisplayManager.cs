d29815cd4c9dd8691ca680edb01bd83361252ae8
jusjustin <justin4@uic.edu>
2020-04-10 20:00:09 -0500

    Remove all
    Remove all

--- Final Project/Assets/module-omicron-master/CAVE2/Examples/CAVESimulator/Scripts/VRDisplayManager.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class VRDisplayManager : MonoBehaviour {
-
-    public Transform headTrackedUser;
-    public Transform virtualHead;
-    public float displayAngularOffset = 1;
-}
--- README.md
+++
@@ -1,4 +0,0 @@
-# cs426_Selective_Detective_Final_Project
-
-Repository: Github
-Bug Tracking: Github
