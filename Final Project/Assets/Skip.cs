a77efd665f7429a8c900aa3539cb053788f5ddee
slambe7@uic.edu <45742163+slambert-cs@users.noreply.github.com>
2020-04-27 23:05:40 -0500

    added skip and quit buttons
    added a skip button for the tutorial and a quit button for the credits scene

---
+++ Final Project/Assets/Skip.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Skip : MonoBehaviour
+{
+    public void SkipScene(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
